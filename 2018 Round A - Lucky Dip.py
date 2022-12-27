1e3ab79029b14ad367f27b24ed354fabd6c8f596
ka1victor <89969071+ka1victor@users.noreply.github.com>
2022-12-27 17:17:58 -0300

    Binary Search Implementation: len_lucky()

--- 2018 Round A - Lucky Dip.py
+++ 2018 Round A - Lucky Dip.py
@@ -1,6 +1,22 @@
+def len_lucky(L,value): #sorted list, integer~
+    start = 0
+    end = len(L)-1
+    mid = int((start+end)/2) #middle for odd length, left-middle for even length
+    while end-start > 0:
+        if L[mid]>value: #-->
+            start = mid
+            #end = end
+            mid = int((start+end)/2)
+        else:
+            #start = start
+            end = mid
+            mid = int((start+end)/2)
+
+        return mid
+
 def expected_value_dip(N,K,V):
     V_list = V.split(" ")
-    V_list = list(map(int,V_list))
+    V_list = list(map(float,V_list))
 
     # E(dip) = sum of E(values), which are V[value] * P[value]
 
@@ -25,11 +41,17 @@ def expected_value_dip(N,K,V):
             
             # But the for loop takes O(N) to run => our function takes O(K*N), which is too much time for the large data set. 
             # So we'll use a binary search to calculate for k>1, which will take O(Log N) to run, making our function O(K*Log N).
-            sorted_list = sort(V_list)
+
+            V_list = sort(V_list)
+            n_lucky = len_lucky(V_list,E_list[k-1])
+            n_unlucky = N-n_lucky
+            E_k = E_list[k-1]*(n_unlucky/N)
+            for v in V_list[0:n_lucky-1]:
+                E_k = E_k + v/N
             
             E_list.append(E_k)
 
-    return str(float(E_list[-1]))
+    return str(E_list[-1])
 
 T = int(input())
 NK = []
